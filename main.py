a96fa69b2f5c37ed91aef639fbc80574d46beaff
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-14 00:08:17 +0530

    Update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Action(Enum):
     ENTER = "enter"
     LEAVE = "leave"
 
+
 @app.get('/{id}/{action}')
 async def root(id:str,action:Action):
     url = f"https://smart-attendence.deta.dev/{id}/{action.value}"
